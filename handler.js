efb24633da0f9a439857f17b73a24b576302d9ff
jmfinch <92829949+jmfinch@users.noreply.github.com>
2023-02-05 01:17:59 -0700

    feat(handler): add cors handling (#3)
    Add cors handling

--- handler.js
+++ handler.js
@@ -6,6 +6,19 @@ const bodyParser = require('body-parser');
 const deviceRoutes = require('./handler/routes/device');
 const unhandledRoutes = require('./handler/routes/unhandled');
 
+handler.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Contro-Allow-Methods', 'GET, POST')
+        return res.status(200).json({});
+    }
+    next();
+})
+
 handler.use(bodyParser.urlencoded({extended: false}));
 handler.use(bodyParser.json());
 
