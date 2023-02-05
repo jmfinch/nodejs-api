8afac7eec6f8411005f286e7844e8fd8b7f43682
jmfinch <92829949+jmfinch@users.noreply.github.com>
2023-02-05 01:03:33 -0700

    feat(parser): add bodyparser (#2)
    Add bodyParser for handling request bodies.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nodejs-api
 NodeJS API
+
+Miro diagram: https://miro.com/app/board/uXjVPqyWFI0=/?share_link_id=288699689830
--- handler.js
+++ handler.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const res = require('express/lib/response');
 const handler = express();
+const bodyParser = require('body-parser');
 
 const deviceRoutes = require('./handler/routes/device');
 const unhandledRoutes = require('./handler/routes/unhandled');
 
-handler.use('/device', deviceRoutes);
+handler.use(bodyParser.urlencoded({extended: false}));
+handler.use(bodyParser.json());
 
-handler.use('/', unhandledRoutes)
+handler.use('/device', deviceRoutes);
+handler.use(unhandledRoutes)
 
 module.exports = handler;
